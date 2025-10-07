04deb713b20b0a291bc56499f9c7d68213b6a515
DongxuanHe <101452009+DongxuanHe@users.noreply.github.com>
2025-10-07 02:12:09 +0000

    Deploying to gh-pages from @ DongxuanHe/DongxuanHe.github.io@8400426b2810ee8e1bd5373f2b0de5b07624ddb7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,21 +501,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68%65%64%78%73%7A%7A%78@%6F%75%74%6C%6F%6F%6B.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/DongxuanHe", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/dongxuan-he-311739225", "_blank");
         },
       },{
         id: 'social-scholar',
@@ -525,11 +525,11 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-rss',
+        title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("/feed.xml", "_blank");
         },
       },{
       id: 'light-theme',
