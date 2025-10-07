10abd8402fced2216c02df1fbe519a2f8751f4f2
DongxuanHe <101452009+DongxuanHe@users.noreply.github.com>
2025-10-07 03:01:05 +0000

    Deploying to gh-pages from @ DongxuanHe/DongxuanHe.github.io@1c17f262e6edaae213cf8f68ea0fa7638b526dba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -426,12 +426,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-",
+          title: '',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "projects-project-1",
+          section: "News",},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
