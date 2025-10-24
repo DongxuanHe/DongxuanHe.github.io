68c20131a4c263971dd0a58e6ef8df9902076b95
DongxuanHe <101452009+DongxuanHe@users.noreply.github.com>
2025-10-24 20:39:19 +0000

    Deploying to gh-pages from @ DongxuanHe/DongxuanHe.github.io@0aaacf33b62fb95b18c5df317e24153ed59fcc2d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -70,16 +70,16 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/master/";
-            },},{id: "projects-multi-agent-reinforcement-learning-and-sim2real-transfer",
-          title: 'Multi-Agent Reinforcement Learning and Sim2Real Transfer',
-          description: "Bridging simulation and real-world domains in multi-agent learning",
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-reinforcement-learning-for-uav-obstacle-avoidance",
-          title: 'Reinforcement Learning for UAV Obstacle Avoidance',
-          description: "Training autonomous UAVs to navigate from point A to B while avoiding randomly placed obstacles",
+              window.location.href = "/projects/7_project/";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
+              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
@@ -100,21 +100,21 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-reinforcement-learning-for-uav-obstacle-avoidance",
+          title: 'Reinforcement Learning for UAV Obstacle Avoidance',
+          description: "Training autonomous UAVs to navigate from point A to B while avoiding randomly placed obstacles",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-multi-agent-reinforcement-learning-and-sim2real-transfer",
+          title: 'Multi-Agent Reinforcement Learning and Sim2Real Transfer',
+          description: "Bridging simulation and real-world domains in multi-agent learning",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
             },},{
         id: 'social-email',
         title: 'email',
