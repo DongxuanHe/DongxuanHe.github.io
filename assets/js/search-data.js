f8d454e68e506af877e02d3d63a4ea623ea7b32e
DongxuanHe <101452009+DongxuanHe@users.noreply.github.com>
2025-10-24 20:47:08 +0000

    Deploying to gh-pages from @ DongxuanHe/DongxuanHe.github.io@2f1147c86a177bf8045e00b11d8c9c2266134bb3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -100,11 +100,6 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
@@ -115,6 +110,11 @@ ninja.data = [{
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
+            },},{id: "projects-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
