6ffc63414243e31d4d83a61de2c19d5366440cd7
DongxuanHe <101452009+DongxuanHe@users.noreply.github.com>
2025-10-24 20:44:30 +0000

    Deploying to gh-pages from @ DongxuanHe/DongxuanHe.github.io@35b914f2f4cc1167d16a533918991206d64550bc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -75,11 +75,11 @@ ninja.data = [{
           description: "Bridging simulation and real-world domains in multi-agent learning",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-reinforcement-learning-for-uav-obstacle-avoidance",
+          title: 'Reinforcement Learning for UAV Obstacle Avoidance',
+          description: "Training autonomous UAVs to navigate from point A to B while avoiding randomly placed obstacles",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
+              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
@@ -110,11 +110,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "projects-reinforcement-learning-for-uav-obstacle-avoidance",
-          title: 'Reinforcement Learning for UAV Obstacle Avoidance',
-          description: "Training autonomous UAVs to navigate from point A to B while avoiding randomly placed obstacles",
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
+              window.location.href = "/projects/7_project/";
             },},{
         id: 'social-email',
         title: 'email',
